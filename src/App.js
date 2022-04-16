2fd2400bdc275e296bf48c6f8ff7539679421f36
arisanator9900 <96804792+arisanator9900@users.noreply.github.com>
2022-04-16 03:27:04 -0400

    Updated react views and components

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Landing from './views/Landing';
 import Skills from './views/Skills';
 import Experience from './views/Experience';
 import Contact from './views/Contact';
+import Toggle from './views/Toggle'
 
 const App = () => {
 	return (
 		<>
+      <Toggle />
 			<Landing />
 			<About />
 			<Skills />
