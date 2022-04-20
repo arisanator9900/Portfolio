d04d5aaf77b118b7af33559c139177afeff371a3
arisanator9900 <96804792+arisanator9900@users.noreply.github.com>
2022-04-20 04:26:17 -0400

    react router install

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import { ThemeProvider } from './components/context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  
     <ThemeProvider>
       <App />
     </ThemeProvider>
+  
   </React.StrictMode>
 );
 
