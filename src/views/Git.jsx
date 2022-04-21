b15a5a425eb979567b0bf91baee2bdad988fb570
arisanator9900 <96804792+arisanator9900@users.noreply.github.com>
2022-04-21 02:42:33 -0400

    added responsiveness to main pages

--- src/views/Git.jsx
+++ src/views/Git.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import '../css/Git.css'
 import Portfolio from '../components/Portfolio';
 import { portItemsCode } from '../components/portData';
+import Line from '../components/Line';
 
 const Git = () => {
     return (
         <div className="g">
           <div className="g-texts">
             <h1 className="g-title">GitHub Projects</h1>
+            <Line/>
             <p className="g-desc">
               Check out what I've been up to recently on my Github! From React applications to
                Vanilla JavaScript to just plain HTML and CSS web Designs.
--- src/views/Graphic.jsx
+++ src/views/Graphic.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import '../css/Git.css'
 import Portfolio from '../components/Portfolio';
 import { portItemsGraphic } from '../components/portData';
+import Line from '../components/Line';
 
 const Git = () => {
     return (
         <div className="g">
           <div className="g-texts">
             <h1 className="g-title">Design Projects</h1>
+            <Line/>
             <p className="g-desc">
               My Behance portfolio shows off a few skills in different areas: Illustrations, UI Design, 
               Graphic design, Photography, Logos and Social Media Content.
